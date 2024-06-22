bea9094ddf55786e3cf0d93f373beaf39afc9da4
zxuhan7 <zxuhan7@gmail.com>
2024-06-22 16:43:18 +0200

    finished hw02

--- hw/hw02/hw02.py
+++ hw/hw02/hw02.py
@@ -32,6 +32,11 @@ def product(n, term):
     162
     """
     "*** YOUR CODE HERE ***"
+    i = 1
+    ans = 1
+    while i <= n:
+        ans, i = ans * term(i), i + 1
+    return ans
 
 
 def accumulate(fuse, start, n, term):
@@ -54,7 +59,12 @@ def accumulate(fuse, start, n, term):
     19
     """
     "*** YOUR CODE HERE ***"
-
+    i = 1
+    ans = start
+    while i <= n:
+        ans, i = fuse(ans, term(i)), i + 1
+    return ans
+    
 
 def summation_using_accumulate(n, term):
     """Returns the sum: term(1) + ... + term(n), using accumulate.
@@ -68,7 +78,7 @@ def summation_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
+    return accumulate(add, 0, n, term)
 
 
 def product_using_accumulate(n, term):
@@ -83,7 +93,7 @@ def product_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
+    return accumulate(mul, 1, n, term)
 
 
 def make_repeater(f, n):
@@ -100,4 +110,11 @@ def make_repeater(f, n):
     390625
     """
     "*** YOUR CODE HERE ***"
+    def h(x):
+        i = 1
+        while i <= n:
+            x = f(x)
+            i += 1
+        return x
+    return h
 
